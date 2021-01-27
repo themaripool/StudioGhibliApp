3fea825a3e78df50fda590a4bfacebf89717d5ae
MariAndrade <marielama@outlook.com.br>
2021-01-27 17:00:33 -0300

    [CHANGE] Home view app and [ADD] Carousel

--- StudioGhibliApp/StudioGhibliApp/ViewController.swift
+++ StudioGhibliApp/StudioGhibliApp/ViewController.swift
@@ -2,16 +2,41 @@
 //  ViewController.swift
 //  StudioGhibliApp
 //
-//  Created by Mariela Mendonça de Andrade on 26/01/21.
+//  Created by Mariela Andrade on 27/01/21.
 //
 
 import UIKit
+import iCarousel
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, iCarouselDataSource {
 
+    var homeCarousel: iCarousel = {
+        let view = iCarousel()
+        view.type = .rotary
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addSubview(homeCarousel)
+        homeCarousel.dataSource = self
+       // homeCarousel.autoscroll = -0.3
+        
+        homeCarousel.frame = CGRect(x: 0, y: 80, width: view.frame.size.width, height: 400)
+    }
+    
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return 5
+    }
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 350, height: 400))
+        view.backgroundColor = .red
+        
+        let imgView = UIImageView(frame: view.bounds)
+        view.addSubview(imgView)
+        imgView.contentMode = .scaleToFill
+        imgView.image = UIImage(named: "Banners_placeholder\(index+1)")
+        return view
     }
 }
